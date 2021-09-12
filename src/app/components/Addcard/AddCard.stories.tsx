9c80e355cf2b332f5a575a0e59882b9a78128c8c
christophschmitz <c.schmitz@outlook.de>
2021-09-12 12:23:55 +0200

    🐛  Add onClick variable to stories

--- src/app/components/Addcard/AddCard.stories.tsx
+++ src/app/components/Addcard/AddCard.stories.tsx
@@ -11,10 +11,13 @@ export default {
   },
 };
 
+const onClickAdd = () => console.log('Added');
+
 export const addCard = (): JSX.Element => (
   <AddCard
     imageSrc="https://images.unsplash.com/photo-1536329583941-14287ec6fc4e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80"
     title="Handlettering"
     description="500 hrs left"
+    onClick={onClickAdd}
   />
 );
