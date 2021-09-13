45aa838745ef6fa6307b018cf7071328ec69846c
Christoph Schmitz <87307464+christophschmitz@users.noreply.github.com>
2021-09-13 17:30:37 +0200

    Component/ExplorationCard (#38)
    * ✨  Add white typography to Typography component
    
    * 💄  Add style to Typography component
    
    * 🤡  Add Typography stories
    
    * ✨  Add alternative AddIcon component to assets
    
    * ✨  Add Explorationcard component
    
    * 💄  Add style to ExplorationCard component
    
    * 🤡  Add stories to ExplorationCard component
    
    * 🔥  Remove red box around my button

--- src/app/components/Typography/Typography.tsx
+++ src/app/components/Typography/Typography.tsx
@@ -2,15 +2,18 @@ import React from 'react';
 import styles from './Typography.module.css';
 
 export type TypographyProps = {
-  size: 's' | 'm' | 'l' | 'p';
+  size: 's' | 'm' | 'ms' | 'l' | 'p' | 'ps';
   children: React.ReactNode;
+  className?: string;
 };
 
 const styleMap = {
   s: styles.smallHeadline,
   m: styles.mediumHeadline,
+  ms: styles.mediumHeadlineSecondary,
   l: styles.largeHeadline,
   p: styles.paragraph,
+  ps: styles.paragraphSecondary,
 };
 
 export default function Typography({
@@ -22,9 +25,13 @@ export default function Typography({
       return <h3 className={styleMap[size]}>{children}</h3>;
     case 'm':
       return <h2 className={styleMap[size]}>{children}</h2>;
+    case 'ms':
+      return <h2 className={styleMap[size]}>{children}</h2>;
     case 'l':
       return <h1 className={styleMap[size]}>{children}</h1>;
     case 'p':
+      return <p className={styleMap[size]}>{children}</p>;
+    case 'ps':
       return <p className={styleMap[size]}>{children}</p>;
   }
 }
