2c1b6f211751bfa75a14f12b029995fc5aced4ac
christophschmitz <c.schmitz@outlook.de>
2021-09-20 15:41:23 +0200

    ✨  Create route to get single skill

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
-import { readSkills } from './utils/skills';
+import { getSkill, readSkills } from './utils/skills';
 
 const app = express();
 const port = process.env.PORT || 3001;
@@ -9,8 +9,24 @@ const port = process.env.PORT || 3001;
 app.use(express.json());
 
 app.get('/api/skills', async (_request, response) => {
-  const skills = await readSkills();
-  response.status(200).json(skills);
+  try {
+    const skills = await readSkills();
+    response.status(200).json(skills);
+  } catch (error) {
+    console.error(error);
+    response.status(500).send('Internal Server Error! Please try again later.');
+  }
+});
+
+app.get('/api/skills/:title', async (request, response) => {
+  const { title } = request.params;
+  try {
+    const skill = await getSkill(title);
+    response.status(200).json(skill);
+  } catch (error) {
+    console.error(error);
+    response.status(404).send(`Could not find skill: ${title}`);
+  }
 });
 
 app.use('/storybook', express.static('dist/storybook'));
