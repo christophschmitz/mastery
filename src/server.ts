a081916bb35914837451f85ed91eaea128f4f44f
christophschmitz <c.schmitz@outlook.de>
2021-09-20 17:02:24 +0200

    ✨  Add put route to server

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,13 @@ import dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
 import type { Skill } from './types';
-import { getSkill, readSkills, addSkill, deleteSkill } from './utils/skills';
+import {
+  getSkill,
+  readSkills,
+  addSkill,
+  deleteSkill,
+  updateSkill,
+} from './utils/skills';
 
 const app = express();
 const port = process.env.PORT || 3001;
@@ -40,6 +46,18 @@ app.delete('/api/skills/:title', async (request, response) => {
   const { title } = request.params;
   await deleteSkill(title);
   response.status(200).send();
+});
+
+app.put('/api/skills/:title', async (request, response) => {
+  const { title } = request.params;
+  const skill: Skill = request.body;
+  try {
+    await updateSkill(title, skill);
+    response.status(200).json(skill);
+  } catch (error) {
+    console.error(error);
+    response.status(404).send(`Could not find skill: ${title}`);
+  }
 });
 
 app.use('/storybook', express.static('dist/storybook'));
