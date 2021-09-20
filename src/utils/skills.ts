b8b7211a95047d145377198c8999e6849f262583
christophschmitz <c.schmitz@outlook.de>
2021-09-20 15:30:19 +0200

    ✨  Implement getSkills to get a single skill

--- src/utils/skills.ts
+++ src/utils/skills.ts
@@ -1,9 +1,19 @@
 import { readFile } from 'fs/promises';
 import type { Skills, Skill } from '../types';
-
+//All skills
 export async function readSkills(): Promise<Skill[]> {
   const response = await readFile('src/db.json', 'utf-8');
   const db: Skills = JSON.parse(response);
   const skills = db.skills;
   return skills;
+}
+//Single skill
+export async function getSkill(title: string): Promise<Skill> {
+  const skills = await readSkills();
+  const skill = skills.find((skill) => skill.title === title);
+
+  if (!skill) {
+    throw new Error(`No skill found for skill: ${title}`);
+  }
+  return skill;
 }
