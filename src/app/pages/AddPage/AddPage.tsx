27674bdc5b96d4646faa1d9825c3a3384e226fe4
christophschmitz <c.schmitz@outlook.de>
2021-09-23 13:40:47 +0200

    ✨  Update AddSkill Page to use localStorage

--- src/app/pages/AddPage/AddPage.tsx
+++ src/app/pages/AddPage/AddPage.tsx
@@ -4,16 +4,21 @@ import Header from '../../components/Header/Header';
 import TagGroup from '../../components/TagGroup/TagGroup';
 import Card from '../../components/Card/Card';
 import Navigation from '../../components/Navigation/Navigation';
-import useAllSkills from '../../hooks/useAllSkills';
+import useSkills from '../../hooks/useSkills';
 import { CATEGORIES } from '../../components/lib/categoryMap';
 import { useHistory } from 'react-router';
+import useLocalStorageSkills from '../../hooks/useLocalStorageSkills';
+import type { Skill } from '../../../types';
 
 export default function AddPage(): JSX.Element {
   const [selectedCategory, setSelectedCategory] = useState<string | null>(
     'crafting'
   );
-  const { skills } = useAllSkills();
+
   const history = useHistory();
+  const { skills } = useSkills();
+
+  const { addOnClick } = useLocalStorageSkills();
 
   const categoryList = Object.values(CATEGORIES);
   const tagList = categoryList.map((category) => {
@@ -26,50 +31,51 @@ export default function AddPage(): JSX.Element {
     };
   });
 
-  const allCategorys = skills?.map((skills) => {
-    return {
-      title: skills.title,
-      imageSrc: skills.imageSrc,
-      description: skills.description,
-      category: skills.category,
-    };
+  const allCategorys = skills?.map((skill) => {
+    const { title, imageSrc, description, category, _id, progress, isDone } =
+      skill;
+    return { title, imageSrc, description, category, _id, progress, isDone };
   });
   const filteredSkills = allCategorys?.filter(
     (skills) => skills.category === selectedCategory
   );
 
+  function handleAddOnClick(skills: Skill) {
+    addOnClick(skills);
+  }
+
   return (
-    <>
-      <div className={styles.container}>
-        <header className={styles.header}>
-          <Header
-            className={styles.head}
-            type="backwithimage"
-            imageSrc="https://images.unsplash.com/photo-1522075469751-3a6694fb2f61?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=100&q=80"
-            onClick={() => {
-              history.push('/');
-            }}
-          />
-          <TagGroup tagList={tagList} className={styles.tagGroup} />
-        </header>
-        <main className={styles.cardWrapper}>
-          {filteredSkills &&
-            filteredSkills.map((skills) => {
-              return (
-                <Card
-                  type="add"
-                  {...skills}
-                  imageSrc={skills.imageSrc}
-                  title={skills.title}
-                  description={skills.description}
-                />
-              );
-            })}
-        </main>
-        <div>
-          <Navigation activeLink={'add'} />
-        </div>
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <Header
+          className={styles.head}
+          type="backwithimage"
+          imageSrc="https://images.unsplash.com/photo-1522075469751-3a6694fb2f61?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=100&q=80"
+          onClick={() => {
+            history.push('/');
+          }}
+        />
+        <TagGroup tagList={tagList} className={styles.tagGroup} />
       </div>
-    </>
+      <main className={styles.cardWrapper}>
+        {filteredSkills &&
+          filteredSkills.map((skills) => {
+            return (
+              <Card
+                {...skills}
+                id={skills._id}
+                progress={skills.progress}
+                type="add"
+                imageSrc={skills.imageSrc}
+                title={skills.title}
+                description={skills.description}
+                onClick={() => handleAddOnClick(skills)}
+              />
+            );
+          })}
+      </main>
+
+      <Navigation activeLink={'add'} />
+    </div>
   );
 }
