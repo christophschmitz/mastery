f352b11eedb45eb9beb079bdd041ae7078fdc2c9
christophschmitz <c.schmitz@outlook.de>
2021-09-24 11:05:50 +0200

    🐛  Fix brackets

--- src/app/components/Card/Card.stories.tsx
+++ src/app/components/Card/Card.stories.tsx
@@ -18,7 +18,7 @@ export const defaultCard = (): JSX.Element => (
     imageSrc="https://images.unsplash.com/photo-1536329583941-14287ec6fc4e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80"
     title="Handlettering"
     description="500 hrs left"
-    id={'string'}
+    id="string"
     progress={0}
   />
 );
@@ -29,7 +29,7 @@ export const addCard = (): JSX.Element => (
     title="Handlettering"
     description="500 hrs left"
     onClick={onClickAdd}
-    id={'string'}
+    id="string"
     progress={0}
   />
 );
