8e41aa4974a22d72d1c8e70802a7c9ed09fd8638
christophschmitz <c.schmitz@outlook.de>
2021-09-28 14:07:17 +0200

    🏷️  Added progress to CardProps

--- src/app/components/Card/Card.tsx
+++ src/app/components/Card/Card.tsx
@@ -9,10 +9,10 @@ export type CardProps = {
   type: 'default' | 'add';
   imageSrc: string;
   title: string;
-  description: string;
+  description?: string;
   onClick?: () => void;
   id: string | undefined;
-  progress: number;
+  progress: string | number;
 };
 
 const Card = ({
@@ -20,16 +20,25 @@ const Card = ({
   imageSrc,
   title,
   description,
+  progress,
   onClick,
 }: CardProps): JSX.Element => {
   return (
     <li>
       <article className={styles.card}>
         <img src={imageSrc} alt="placeholder image" className={styles.imgage} />
-        <div className={styles.content}>
-          <Typography size="m">{title}</Typography>
-          <Typography size="p">{description}</Typography>
-        </div>
+        {type === 'default' ? (
+          <div className={styles.content}>
+            <Typography size="m">{title}</Typography>
+            <Typography size="p">{`${progress} hrs left`}</Typography>
+          </div>
+        ) : (
+          <div className={styles.content}>
+            <Typography size="m">{title}</Typography>
+            <Typography size="p">{description}</Typography>
+          </div>
+        )}
+
         {type === 'default' ? (
           <div className={styles.chevron}>
             <Link to={`/details${title}`}>
