b8a6084e5ad7c89d7713cfc95d75ed66f371b08f
christophschmitz <c.schmitz@outlook.de>
2021-10-01 08:04:48 +0200

    💄  Change pathNames in SVGs

--- src/app/components/assets/AddIcon.tsx
+++ src/app/components/assets/AddIcon.tsx
@@ -12,8 +12,8 @@ const AddIcon = (props: React.SVGProps<SVGSVGElement>): JSX.Element => {
     >
       <circle cx="18" cy="18" r="18" fill="white" />
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M19 11C19 10.4477 18.5523 10 18 10C17.4477 10 17 10.4477 17 11V17H11C10.4477 17 10 17.4477 10 18C10 18.5523 10.4477 19 11 19H17V25C17 25.5523 17.4477 26 18 26C18.5523 26 19 25.5523 19 25V19H25C25.5523 19 26 18.5523 26 18C26 17.4477 25.5523 17 25 17H19V11Z"
         fill="#12CDF5"
       />
